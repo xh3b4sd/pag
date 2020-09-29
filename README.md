d7f2b2ebcfb42f4066b7d86c67fc99d4973d434c
Tim Schindler <xh3b4sd@gmail.com>
2020-09-29 22:19:34 +0200

    setup project structure (#1)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pag
-opinionated protocol buffer api code generator
+
+Opinionated protocol buffer api code generator. `pag` stands for "proto api
+generator". The tool wraps the necessary business logic of generating [grpc]
+code based on a [protocol buffer] API scheme.
+
+
+
+[grpc]: https://grpc.io
+[protocol buffer]: https://developers.google.com/protocol-buffers
---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description = "Opinionated protocol buffer api code generator."
+	gitSHA      = "n/a"
+	name        = "pag"
+	source      = "https://github.com/xh3b4sd/pag"
+	version     = "n/a"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
