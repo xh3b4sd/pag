23a0c42353baf0b14ac14b280ef7764e96bdf71a
Tim Schindler <xh3b4sd@gmail.com>
2021-01-19 18:25:21 +0100

    allow experimental feature for optional fields (#29)

--- pkg/generate/golang/golang.go
+++ pkg/generate/golang/golang.go
@@ -18,12 +18,12 @@ const (
 	// structs based on gRPC messages as of time of writing this. The code
 	// generation process is separate due to API changes and migration efforts
 	// in the upstream gRPC ecosystem.
-	MsgArg = "--go_out=%s/ --proto_path=%s %s"
+	MsgArg = "--experimental_allow_proto3_optional --go_out=%s/ --proto_path=%s %s"
 	// SvcArg is the specific argument string required in order to generate go
 	// interfaces based on gRPC services as of time of writing this. The code
 	// generation process is separate due to API changes and migration efforts
 	// in the upstream gRPC ecosystem.
-	SvcArg = "--go-grpc_out=%s/ --proto_path=%s %s"
+	SvcArg = "--experimental_allow_proto3_optional --go-grpc_out=%s/ --proto_path=%s %s"
 )
 
 type Config struct {
